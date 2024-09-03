af8fdb38e82482592d3ded115a086d215e10423b
62057 <kr.adiraj0301@gmail.com>
2024-09-03 13:08:37 +0530

    First commit

---
+++ Hello.py
@@ -0,0 +1,29 @@
+# Using basic arithmetic operation.
+a=9
+b=5
+
+print("Addition :",a+b)
+print("Multiplication :",a*b)
+print("Fraction Quotient :",a/b)
+print("Subtraction :",a-b)
+print("Remainder :",a%b)
+print("Integer Quotient :",a//b)
+print("Remainder on diving ",a,"by",b,"is",a%b)
+
+s='hello'
+s1='India And Australia'
+
+print(s.upper())
+print(s.replace('l','o'))
+
+print(s1.upper())
+print(s1.lower())
+
+print(s1.replace('India','Bihar'))
+
+print(a==b)
+print(a!=b)
+print(a<b)
+print(a>b)
+
+print(a or b)
